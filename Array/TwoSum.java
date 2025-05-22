9c13179e9c3816facbc949393cfbbba44fce9fc1
yerus <surendarraja420@gmail.com>
2025-05-22 19:25:31 +0300

    feat:RemoveDuplicate

---
+++ Array/TwoSum.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    private static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int sum = target - nums[i];
+            if (map.containsKey(sum)) {
+                return new int[]{map.get(sum), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        Arrays.stream(twoSum(nums, target)).forEach(System.out::print);
+    }
+}
