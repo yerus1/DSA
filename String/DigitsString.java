622c060278474b362825ba987f8b4e0559e027ac
yerus <surendarraja420@gmail.com>
2025-05-24 06:11:49 +0300

    leet:DigitsString

--- String/DigitsString.java
+++ String/DigitsString.java
@@ -7,17 +7,18 @@ public class DigitsString {
         for (Character c : s.toCharArray()) {
             str.append(c - 'a' + 1);
         }
-        String transform= str.toString();
-        return sumDigit(transform,k);
+        String transform = str.toString();
+        return sumDigit(transform, k);
     }
-    public static int sumDigit(String str,int k) {
-        int sum=0;
+
+    public static int sumDigit(String str, int k) {
+        int sum = 0;
         for (Character c : str.toCharArray()) {
             sum = sum + c - '0';
         }
         k--;
-        str=String.valueOf(sum);
-        return k == 0 ? sum : sumDigit(str, k );
+        str = String.valueOf(sum);
+        return k == 0 ? sum : sumDigit(str, k);
     }
 
     public static void main(String[] args) {
