1d2e23b576c5b522c5560e26bee38a9370a4f56a
yerus <surendarraja420@gmail.com>
2025-05-24 21:03:08 +0300

    leetcode:binarySearch

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 3, 5, 9, 12};
+        int target = 1;
+        System.out.println(binarySearch(nums, target));
+    }
+}
