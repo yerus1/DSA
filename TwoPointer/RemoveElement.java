961cb8893aac7a81369b79c79de648031ba08f2f
yerus <surendarraja420@gmail.com>
2025-05-26 07:23:08 +0300

    leetcode:removeElement

--- TwoPointer/RemoveElement.java
+++ TwoPointer/RemoveElement.java
@@ -1,20 +1,24 @@
 package TwoPointer;
 
+import java.util.Arrays;
+
 public class RemoveElement {
 
     public static int removeElement(int[] nums, int val) {
-        int left =0;
-        int right=nums.length;
-        int count=0;
-        while(left<right){
-
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
         }
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
         int[] num = {3, 2, 2, 3};
         int val = 3;
-        System.out.println(removeElement(num, val));
+        int len = removeElement(num, val);
+        System.out.println(len);
+        Arrays.stream(num).limit(len).forEach(System.out::print);
     }
 }
