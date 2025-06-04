4ae717cb28e6980e60d548eb6f70b9f2c03a0443
Neo <surendarraja420@gmail.com>
2025-06-04 18:22:31 +0530

    DSA:FindZero

--- Recursion/Basics/FindZero.java
+++ Recursion/Basics/FindZero.java
@@ -4,17 +4,17 @@ public class FindZero {
 
 
     static int findZero(int n) {
-        return count(n,0);
+        return count(n, 0);
     }
 
-    static int count(int n,int c) {
-        if(n==0) return c;
+    static int count(int n, int c) {
+        if (n == 0) return c;
 
-        int rem=n%10;
-        if(rem==0) {
-            return count(n/10,c+1);
+        int rem = n % 10;
+        if (rem == 0) {
+            return count(n / 10, c + 1);
         }
-        return count(n/10,c);
+        return count(n / 10, c);
     }
 
     public static void main(String[] args) {
