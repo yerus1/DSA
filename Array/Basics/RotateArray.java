fb67855cf7824caeddb504d0fabd636ba5331ef1
Neo <surendarraja420@gmail.com>
2025-06-05 17:38:47 +0530

    DSA:RotateArray

---
+++ Array/Basics/RotateArray.java
@@ -0,0 +1,26 @@
+package Array.Basics;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    static void rotateArray(int[] arr,int k){
+        reverse(arr,0, arr.length-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,arr.length-1);
+    }
+
+    static  void reverse(int[] nums,int left,int right){
+        while(left<right){
+            int temp=nums[left];
+            nums[left++]=nums[right];
+            nums[right--]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotateArray(nums,k);
+        Arrays.stream(nums).forEach(System.out::print);
+    }
+}
