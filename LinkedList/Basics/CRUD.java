598772f3ee9c2118282cd874366b202890da3736
Neo <surendarraja420@gmail.com>
2025-06-10 22:19:02 +0530

    DSA:NodeInsert

--- LinkedList/Basics/CRUD.java
+++ LinkedList/Basics/CRUD.java
@@ -13,5 +13,28 @@ class Node {
 
 public class CRUD {
 
+    static Node head;
 
+    static Node insert(int i) {
+        Node node = new Node(i);
+        node.next = head;
+        return head = node;
+    }
+
+    static void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            insert(i);
+        }
+        print();
+    }
 }
